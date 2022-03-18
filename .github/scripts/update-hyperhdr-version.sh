d58c71ee073dfe16e0b88cf7632819266676a380
Iona Hrapsa <hrapsaiona@gmail.com>
2022-03-19 00:04:07 +0200

    Update update-hyperhdr-version.sh

--- .github/scripts/update-hyperhdr-version.sh
+++ .github/scripts/update-hyperhdr-version.sh
@@ -7,8 +7,11 @@ CURRENT="$(jq -r ".version" $FILE)"
 
 echo "VERSION=${CURRENT}" >> $GITHUB_ENV
 
-REPO="https://github.com/awawa-dev/HyperHDR.git"
-RELEASE="$(git ls-remote --sort='v:refname' --tags ${REPO} | cut -d/ -f3- | tail -n1)"
+REPO_API="https://api.github.com/repos/awawa-dev/HyperHDR/releases/latest"
+RELEASE="$(curl --silent ${REPO_API} | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
+
+# REPO="https://github.com/awawa-dev/HyperHDR.git"
+# RELEASE="$(git ls-remote --sort='v:refname' --tags ${REPO} | cut -d/ -f3- | tail -n1)"
 
 echo "RELEASE=${RELEASE}" >> $GITHUB_ENV
 
